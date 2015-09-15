d79b995f38a9d1020db1227e04865d9385d5f4e9
Chris Rohlf <chris.rohlf@leafsr.com>
2015-09-15 13:16:47 -0400

    example tool for finding external proxies on your network

---
+++ proxy_scan.cpp
@@ -0,0 +1,43 @@
+// Copyright 2015 Yahoo Inc.
+// Licensed under the BSD license, see LICENSE file for terms.
+// Written by Chris Rohlf
+
+// An example tool that uses Mathilda
+// g++ -o proxy_scan proxy_scan.cpp mathilda.cpp -std=c++11 -ggdb -lcurl -luv
+#include "mathilda.h"
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+vector<std::string> read_file(char *f) {
+	std::ifstream file(f);
+	std::string line;
+	vector <std::string> ret;
+
+	while(std::getline(file, line))
+		ret.push_back(std::move(line));
+
+	return ret;
+}
+
+int main(int argc, char *argv[]) {
+	unique_ptr<Mathilda> m(new Mathilda());
+
+	vector <std::string> hosts;
+	hosts = read_file("potential_proxy_hosts.txt");
+
+	for(auto y : hosts) {
+		Instruction *i = new Instruction((char *) y.c_str(), "host.you.want.to.proxy.to.example.com");
+		i->proxy = y;
+		i->proxy_port = 80;
+		i->use_proxy = true;
+		m->add_instruction(i);
+	}
+
+	m->safe_to_fork = true;
+	m->execute_instructions();
+
+	return OK;
+}
